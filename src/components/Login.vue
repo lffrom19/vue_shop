3d0b410bb9163866078cb1a872ccfc6788d7dff0
L F <you@example.com>
2020-03-19 12:45:51 +0800

    '第一次提交'

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,17 +2,24 @@
   <div class="login_container">
     <div class="login_box">
       <div class="avatar_box">
-        <img src="../assets/img/avatar.jpg">
+        <img src="../assets/img/avatar.jpg" />
       </div>
       <el-form label-width="0px" class="login_form" :model="form">
         <el-form-item>
-          <el-input prefix-icon="iconfont icon-people_fill" v-model="form.username"></el-input>
+          <el-input
+            prefix-icon="iconfont icon-people_fill"
+            v-model="form.username"
+          ></el-input>
         </el-form-item>
-         <el-form-item>
-          <el-input prefix-icon="iconfont icon-lock_fill" v-model="form.password" type="password"></el-input>
+        <el-form-item>
+          <el-input
+            prefix-icon="iconfont icon-lock_fill"
+            v-model="form.password"
+            type="password"
+          ></el-input>
         </el-form-item>
-         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
           <el-button type="info">注册</el-button>
         </el-form-item>
       </el-form>
@@ -25,10 +32,13 @@ export default {
   data () {
     return {
       form: {
-        username: '',
-        password: ''
+        username: 'admin',
+        password: '123456'
       }
     }
+  },
+  methods: {
+    login () {}
   }
 }
 </script>
@@ -46,7 +56,7 @@ export default {
   position: absolute;
   left: 50%;
   top: 50%;
-  transform: translate(-50%,-50%);
+  transform: translate(-50%, -50%);
   border: 2px solid #000;
   .avatar_box {
     height: 130px;
@@ -55,17 +65,17 @@ export default {
     border-radius: 50%;
     position: absolute;
     left: 50%;
-    transform: translate(-50%,-50%);
+    transform: translate(-50%, -50%);
     img {
       width: 100%;
       height: 100%;
-      border-radius: 50%
+      border-radius: 50%;
     }
   }
 }
 .btns {
   display: flex;
-  justify-content: flex-end
+  justify-content: flex-end;
 }
 .login_form {
   position: absolute;
